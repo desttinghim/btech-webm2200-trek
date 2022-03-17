696976c8237ccd9de14bb944fae4a089b1bb589a
Louis Pearson <louispearson@librem.one>
2022-03-17 02:06:09 -0600

    Final tweaks

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,57 @@
 <script>
  import "../app.css";
+ import Navbar from "./navbar.svelte";
+
+ const menuItems = [
+     {name: "Home", link: "/", hideInNavbar: true},
+     {name: "News", link: "#!"},
+     {name: "Contact", link: "#!"},
+     {name: "Support", link: "#!"},
+     {name: "Shop", link: "#!"},
+ ];
 </script>
+
+<header>
+    <Navbar
+        search
+        home={{name: "Trek", link: "/"}}
+        item={menuItems}
+    />
+</header>
+
 <slot />
+
+<footer class="px-4
+               pt-4
+               md:pt-2
+               space-y-4
+               bg-white
+               rounded-t-lg
+               dark:bg-gray-800
+               text-gray-500
+               dark:text-gray-400
+               ">
+    <div class="md:flex
+                md:items-center
+                md:justify-between
+                ">
+        <!-- Copyright Notice -->
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2022 <a href="https://louispearson.work" class="hover:underline">Louis Pearson</a>. CC-BY-AT-SA.
+        </span>
+        <!-- Navigation -->
+        <nav>
+            <ul class="text-base text-gray-500 dark:text-gray-400 py-2 md:py-0
+                       md:flex md:items-center md:justify-between">
+                {#each menuItems as menu}
+                {#if !menu.hideInFooter}
+                <li>
+                    <a class="md:px-4 py-2 block hover:text-orange-400"
+                        href="{menu.link}">{menu.name}</a>
+                </li>
+                {/if}
+                {/each}
+            </ul>
+        </nav>
+    </div>
+</footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,65 +1,66 @@
 <script>
- import Layout from "./_layout.svelte";
  import Image from "./image.svelte"
 </script>
 <!-- Help from https://tailwindcomponents.com/component/hero-with-background-image -->
-<Layout>
-    <div slot="header"
-         class="w-full h-96
-               bg-top bg-cover bg-[url(/mountain-bike.jpg)]">
-        <div class="flex items-center justify-center
-                    w-full h-full
-                    bg-gray-900 bg-opacity-50">
-            <div class="text-center">
-                <h1 class="text-2xl font-semibold text-white uppercase
-                           lg:text-3xl">
-                    Choose A Trek Bike For Your Next
-                    <div class="text-7xl">Adventure</div>
-                </h1>
-                <button class="w-full px-4 py-2 mt-4 lg:w-auto rounded-md
-                               text-base font-semibold text-white uppercase
-                               transition-colors duration-200 transform
-                               bg-orange-600 hover:bg-orange-500 focus:outline-none focus:bg-orange-500">
-                    Shop Now
-                </button>
-            </div>
+<header
+        class="w-full h-96
+            bg-top bg-cover bg-[url(/mountain-bike.jpg)]">
+    <div class="flex items-center justify-center
+                w-full h-full
+                bg-gray-900 bg-opacity-50">
+        <div class="text-center">
+            <h1 class="text-2xl font-semibold text-white uppercase
+                        lg:text-3xl">
+                Choose A Trek Bike For Your Next
+                <div class="text-7xl">Adventure</div>
+            </h1>
+            <button class="w-full px-4 py-2 mt-4 lg:w-auto rounded-md
+                            text-base font-semibold text-white uppercase
+                            transition-colors duration-200 transform
+                            bg-orange-600 hover:bg-orange-500 focus:outline-none focus:bg-orange-500">
+                Shop Now
+            </button>
         </div>
     </div>
-    <div class="p-4 flex items-center space-x-4 w-full h-full">
-        <div class="p-4 bg-white rounded w-full h-full text-center space-y-4">
-            <Image src="/bike-shop-16x9.jpg" alt="A man in a shop surrounded by bikes.">
-                <span slot="attribution">Photo by <a href="https://unsplash.com/@tom607?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Tom Austin</a> on <a href="https://unsplash.com/collections/zN2b34UB6vk/intro-to-design---trek-bikes?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>
-            </Image>
-            <p class="font-semibold text-xl">Find out about our latest happenings!</p>
-            <p class="text-justify">We like to keep our customers up to date with the awesome bikes we're building by sending out a newsletter.</p>
-            <button class="p-2 bg-orange-500 rounded text-white">Read Newsletter</button>
-        </div>
-        <div class="p-4 bg-white rounded w-full h-full text-center space-y-4">
-
-            <Image src="/broken-bike-16x9.jpg" alt="A bike missing it's tires and seatpost." >
-                <span slot="attribution">Photo by <a href="https://unsplash.com/@lg17?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Lance Grandahl</a> on <a href="https://unsplash.com/collections/zN2b34UB6vk/intro-to-design---trek-bikes?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>
-            </Image>
-            <p class="font-semibold text-xl">Need help with your bike?</p>
-            <p class="text-justify">We're proud of the quality of our bikes, but life happens. Visit our support center and get help to continue your adventure!</p>
-            <button class="p-2 bg-orange-500 rounded text-white">Contact Support</button>
-        </div>
+</header>
+<!-- Attribution -->
+<div class="bg-red-500 px-4 py-1 font-medium text-white text-right">
+    <p class="text-sm">Mountain Bike Photo by <a href="https://unsplash.com/@dhika88?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Andhika Soreng</a> on <a href="https://unsplash.com/s/photos/mountain-bike?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
+</div>
+<!-- Calls to action -->
+<div class="p-4 grid grid-cols-1 md:grid-cols-2 gap-4 w-full h-full">
+    <div class="p-4 bg-white rounded text-center space-y-4
+                flex flex-col">
+        <Image src="/bike-shop-16x9.jpg" alt="A man in a shop surrounded by bikes.">
+            <span slot="attribution">Photo by <a href="https://unsplash.com/@tom607?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Tom Austin</a> on <a href="https://unsplash.com/collections/zN2b34UB6vk/intro-to-design---trek-bikes?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>
+        </Image>
+        <p class="font-semibold text-xl">Find out about our latest happenings!</p>
+        <p class="text-justify grow">We like to keep our customers up to date with the awesome bikes we're building by sending out a newsletter.</p>
+        <button class="p-2 bg-orange-500 rounded text-white">Read Newsletter</button>
     </div>
-    <div class="px-4 pb-4 w-full h-full">
-        <form on:submit|preventDefault
-            class="p-4 bg-gray-100 rounded w-full h-full text-center">
-            <label for="email"
-                   class="text-xl font-semibold" >Sign up for our Newsletter!</label>
-            <p class="pb-4">Get our latest accomplishments directly in your email.</p>
-            <span class="flex justify-between">
-                <input type="text" name="email" value="" placeholder="Your Email"
-                    class="rounded-l w-full p-2 focus:rounded-r" />
-                <button type="submit"
-                        class="rounded-r bg-orange-500 p-2 text-white focus:rounded-l" >
-                    Subscribe</button>
-            </span>
-        </form>
+    <div class="p-4 bg-white rounded text-center space-y-4
+                flex flex-col">
+        <Image src="/broken-bike-16x9.jpg" alt="A bike missing it's tires and seatpost." >
+            <span slot="attribution">Photo by <a href="https://unsplash.com/@lg17?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Lance Grandahl</a> on <a href="https://unsplash.com/collections/zN2b34UB6vk/intro-to-design---trek-bikes?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>
+        </Image>
+        <p class="font-semibold text-xl">Need help with your bike?</p>
+        <p class="text-justify grow">We're proud of the quality of our bikes, but life happens. Visit our support center and get help to continue your adventure!</p>
+        <button class="p-2 bg-orange-500 rounded text-white">Contact Support</button>
     </div>
-    <div slot="footer">
-        <p class="text-sm">Mountain Bike Photo by <a href="https://unsplash.com/@dhika88?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Andhika Soreng</a> on <a href="https://unsplash.com/s/photos/mountain-bike?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
-    </div>
-</Layout>
+</div>
+<!-- Newsletter Signup -->
+<div class="px-4 pb-4 w-full h-full">
+    <form on:submit|preventDefault
+        class="p-4 bg-gray-100 rounded w-full h-full text-center">
+        <label for="email"
+                class="text-xl font-semibold" >Sign up for our Newsletter!</label>
+        <p class="pb-4">Get our latest accomplishments directly in your email.</p>
+        <span class="flex justify-between">
+            <input type="text" name="email" value="" placeholder="Your Email"
+                class="rounded-l w-full p-2 focus:rounded-r" />
+            <button type="submit"
+                    class="rounded-r bg-orange-500 p-2 text-white focus:rounded-l" >
+                Subscribe</button>
+        </span>
+    </form>
+</div>
